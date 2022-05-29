8f69bd0e118877c959d474246aadea3d4c4a4735
Vahid V <vhid.vz@gmail.com>
2022-05-29 16:59:29 +0430

    update

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,26 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from '../src/app.module';
+import * as request from 'supertest';
+
+describe('AuthController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/auth/login (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/auth/login')
+      .set('Accept', 'application/json')
+      .send({ username: 'test', password: 'test' })
+      .expect('Content-Type', /json/)
+      .expect(201);
+  });
+});
